ae80e2c5509796748c3cb6b412cd99ef9b9308dc
18011129 Lorenz <xvq7775@autuni.ac.nz>
2025-05-31 03:29:11 +1200

    Initial project setup with Maven structure and team documentation

---
+++ src/main/java/com/cafe/inventory/CafeInventorySystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cafe.inventory;
+
+/**
+ *
+ * @author Enzo
+ */
+public class CafeInventorySystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
